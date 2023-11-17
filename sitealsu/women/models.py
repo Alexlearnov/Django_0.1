88ab890508bec24660219eb87ada2473c5f64a89
Alex <Alexorierwe@protonmail.com>
2023-11-17 22:48:54 +0300

    models

--- sitealsu/women/models.py
+++ sitealsu/women/models.py
@@ -16,10 +16,18 @@ class User(models.Model):
     birthday = models.DateField()
     time_created = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
 
     def __str__(self):
         return f"{self.first_name}, {self.last_name}, {self.slug}, " \
                f"time_created={self.time_created}, birthday={self.birthday}"
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
 
 
 
